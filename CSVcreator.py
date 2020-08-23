175a26ededbd9447c2759ee4d0dfe1fc6c0be47b
Diciannovesimo <williamcolucci1998@gmail.com>
2020-08-23 10:51:32 +0200

    aggiunta la possibilità di visualizzare per team di lavoro

--- CSVcreator.py
+++ CSVcreator.py
@@ -5,41 +5,43 @@ from selenium import webdriver
 print("Analisi di rilevanza di articoli scientifici mediante visualizzazioni")
 """
 variable declarations
-count:         for the csv file
-numbers_paper: for numbers of papers that the user want to view
-MAX:           number of documents the author has written
-author:        select an author in the semantic scholar database
-name:          author name
-file:          the csv file
-CSVstring:     string to write to the CSV file
+count:              ID for every paper
+numbers_of_papers:  for numbers of papers that the user want to view
+MAX:                number of documents the author has written
+author:             select an author in the semantic scholar database
+name:               author name
+file:               the csv file
+CSVstring:          string to write to the CSV file
+auth_name_file:     name of the file, compose of iD and author name
+team:               group of people who worked on the same document
 """
 print("inserisci l'id dell'autore che si vuole analizzare")
 print("esempio: Turing 2262347")
 
-number_auth = input()
-author = sch.author(number_auth)
+iD_auth = input()
+author = sch.author(iD_auth)
 try:
     MAX = int(len(author['papers']))
     pass
 except KeyError:
-    print("Numero autore non valido: " + number_auth)
+    print("Numero autore non valido: " + iD_auth)
     print("Il programma verrà interrotto...")
-    time.sleep(3)
+    time.sleep(2)
     exit()
 
 
 print("quanti documenti scientifici vuoi analizzare?")
 print("documenti totali di questo autore: " + str(MAX))
-numbers_paper = int(input())
-if(numbers_paper < 0 or numbers_paper > MAX):
+numbers_of_papers = int(input())
+if(numbers_of_papers < 0 or numbers_of_papers > MAX):
     print("Numero non valido, verranno analizzati tutti i documenti")
-    numbers_paper = MAX
+    numbers_of_papers = MAX
 
 count = 0
 name = author['name']
 CSVstring = ""
-auth_name_file = number_auth + '_' + name + '_author.csv'
-file = open( number_auth + '_' + name + '_author.csv' , 'w' , encoding='utf8')
+auth_name_file = iD_auth + '_' + name + '_author.csv'
+file = open( auth_name_file, 'w' , encoding='utf8')
 
 print("Analisi in corso...")
 for paper in author['papers']:
@@ -47,21 +49,22 @@ for paper in author['papers']:
     paper_year = paper['year']
     if(paper_year != None):
         paper_title = str(paper['title'])
-        paper_title = paper_title.replace(',','').replace('.','')
+        paper_title = paper_title.replace(',','')
 
         count+=1
         CSVstring = str(count) + "," + name + "," + str(paper_year) + "," + paper_title
         file.write(CSVstring)
 
         paper = sch.paper(paper['paperId'])
+        team = "team_" + str(count)
         for author in paper['authors']:
             if(str(author['name']) != name):
                 file.write("\n")
-                CSVstring = str(count) + "," + str(author['name']) + "," + str(paper_year) + "," + paper_title
+                CSVstring = str(count) + "," + str(author['name']) + "," + str(paper_year) + "," + paper_title + "," + team
                 file.write(CSVstring)
 
         print("Analisi documento numero: " + str(count))
-        if(count > numbers_paper-1):
+        if(count > numbers_of_papers-1):
             print("Analisi completata")
             break
 
@@ -69,9 +72,13 @@ for paper in author['papers']:
     else:
         print("Questo documento: " + str(paper['title']) + " ha una data invalida")
         count+=1
-        if(count > numbers_paper-1):
+        if(count > numbers_of_papers-1):
             print("Analisi completata")
             break
+        else:
+            count-=1
+            numbers_of_papers-=1
+
 
 file.close()
 
@@ -84,17 +91,17 @@ input_file: csv file
 button:     element of the paohvis site
 """
 print("apertura del sito paohvis in corso...")
-time.sleep(4)
+time.sleep(2)
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
 
 driver.get("http://www.di.uniba.it/~buono/paohvis/paoh.html")
-time.sleep(1)
+time.sleep(2)
 
 input_file = driver.find_element_by_xpath('//*[@id="inputFileOpen"]')
 input_file.send_keys("D:/github/Tirocinio/" + auth_name_file)
-time.sleep(1)
+
 
 button = driver.find_element_by_xpath('//*[@id="btnheatmapbsp"]/span')
 button.click()
